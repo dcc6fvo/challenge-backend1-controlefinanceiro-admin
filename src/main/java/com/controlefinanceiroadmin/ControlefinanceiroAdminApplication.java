7b9a7dce2076da81e013fce6b8db6a32ace5c37c
felipe <fvolpato@gmail.com>
2022-02-02 16:28:47 -0300

    Primeiro commit

---
+++ src/main/java/com/controlefinanceiroadmin/ControlefinanceiroAdminApplication.java
@@ -0,0 +1,13 @@
+package com.controlefinanceiroadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlefinanceiroAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlefinanceiroAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlefinanceiroadmin/ControlefinanceiroAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlefinanceiroadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlefinanceiroAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
